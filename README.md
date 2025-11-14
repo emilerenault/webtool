4d566d700f873b16e8ac87ae3c8fa5b416a81552
emilerenault <150397258+emilerenault@users.noreply.github.com>
2025-11-14 07:25:23 +0100

    update

--- README.md
+++ README.md
@@ -1,8 +1,11 @@
-# Modification typographique
-  ***Mots-clés** : typographie, déformation*<br>
-  [Référence](www.google.com)
+# Générateur de mosaïque
+  ***Mots-clés** : forme, couleur, densité*<br>
+  [Référence]([www.p5js.org/sketches/2225777/])
+## L'idée
 
-## Description
+À partir d'une phrase ou de mots rentrés au préalable par l'utilisateur, celui-ci à la possibilité à travers plusieurs réglages de modifier/déformer les caractères typographiques. Et générer ou utiliser une image d'arrière-plan.
+
+## Description de l'outil
 
 À partir d'une phrase ou de mots rentrés au préalable par l'utilisateur, celui-ci à la possibilité à travers plusieurs réglages de modifier/déformer les caractères typographiques. Et générer ou utiliser une image d'arrière-plan.
 
@@ -12,7 +15,29 @@
 - Modifier la taille de la police
 - Modifier la hauteur de ligne
 - Modifier l'espacement des caractères
-- Modifier l'alignement (aligner à gauche, centrer, etc.)
+- **Modifier l'alignement (aligner à gauche, centrer, etc.)**
+```
+let slider;
+
+function setup() {
+  createCanvas(600, 200);
+  textAlign(CENTER, CENTER);
+  textSize(32);
+
+  slider = createSlider(10, 100, 32); // min, max, valeur de départ
+  slider.position(20, 20);
+}
+
+function draw() {
+  background(240);
+  let size = slider.value();
+  textSize(size);
+  text("Hello p5.js!", width / 2, height / 2);
+}
+
+```
+
+![alt text](image.png)
 
 
   
--- sketch.js
+++ sketch.js
@@ -1,7 +1,57 @@
+// Define strokeHue as a global variable. This variable
+// will be used to color each line.
+let strokeHue = 20;
+let colorOffset = 0; // Nouvelle variable pour décaler les couleurs
+// Ajout des variables pour contrôler les points de la courbe
+let controlX1, controlY1, controlX2, controlY2;
+
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(720, 400);
+  noFill();
+  strokeWeight(2);
+  colorMode(HSB);
+  
+  // Initialisation des points de contrôle
+  resetControlPoints();
 }
 
 function draw() {
-  background(220);
+  describe(
+    'Ten rainbow-colored lines in a bezier curve formation that change when clicked.'
+  );
+
+  background(5);
+
+  for (let i = 0; i < 200; i += 20) {
+    // Modification de la couleur avec offset
+    strokeColor = (i + colorOffset) % 360; // Modulo 360 pour rester dans la gamme HSB
+    stroke(strokeColor, 50, 60);
+
+    bezier(
+      mouseX - i / 2, 0 + i,
+      controlX1, controlY1,  // Premier point de contrôle
+      controlX2, controlY2,  // Deuxième point de contrôle
+      240 - i / 16, 300 + i / 8
+    );
+  }
 }
+
+// Fonction appelée au clic qui modifie aléatoirement les points de contrôle
+function mousePressed() {
+  controlX1 = random(width);
+  controlY1 = random(height);
+  controlX2 = random(width);
+  controlY2 = random(height);
+  
+  // Ajoute un décalage aléatoire aux couleurs
+  colorOffset = random(360);
+}
+
+// Fonction pour réinitialiser les points de contrôle
+function resetControlPoints() {
+  controlX1 = 410;
+  controlY1 = 20;
+  controlX2 = 440;
+  controlY2 = 300;
+}
+
